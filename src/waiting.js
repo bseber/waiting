e6af4f729aa1778bd0f5f5ca5d520334d00d750a
Benjamin Seber <seber@synyx.de>
2017-12-09 11:57:10 +0100

    refactored waiting component to map props to internal state

--- src/waiting.js
+++ src/waiting.js
@@ -4,7 +4,19 @@ export default class Waiting extends React.Component {
     static defaultProps = {
         loading: true,
     };
+
+    constructor(props) {
+        super();
+        this.state = { loading: props.loading };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.loading !== this.props.loading) {
+            this.setState({ loading: nextProps.loading });
+        }
+    }
+
     render() {
-        return this.props.loading ? <div>loading...</div> : null;
+        return this.state.loading ? <div>loading...</div> : null;
     }
 }
